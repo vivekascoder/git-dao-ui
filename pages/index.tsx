1601c66b7da76ffe9ead77bb378ae10d6f8d7fea
vivekascoder <vivekascoder@gmail.com>
2022-09-03 15:33:26 +0530

    feat: cute 3d model landed on the home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,59 @@ import { Box, Heading, Text, Flex, Button } from "@chakra-ui/react";
 import PageLayout from "../layouts";
 import Quote from "../components/Quote";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import Spline from "@splinetool/react-spline";
+import { Application, SPEObject } from "@splinetool/runtime";
+
 /**
  *  Responsible for skeleton of the site.
  */
 export default function Home(): React.ReactNode {
+  const model = useRef<undefined | SPEObject>();
+  const [originalX, setOriginalX] = useState<number>(0);
+  const [originalY, setOriginalY] = useState<number>(0);
+  const [originalZ, setOriginalZ] = useState<number>(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const height = window.pageYOffset;
+      if (model.current) {
+        model.current!.rotation.z = originalZ + (0.3 * height) / 1000;
+      }
+    });
+    window.addEventListener("mousemove", (ev: MouseEvent) => {
+      console.log(model.current);
+      if (!model.current) return;
+      console.log({ y: ev.clientY, yy: ev.y });
+      model.current!.rotation.x = originalY + (0.5 * ev.clientY) / 1000;
+      model.current!.rotation.y = originalX + (0.5 * ev.clientX) / 1000;
+    });
+  }, [originalX, originalY, originalZ]);
+
+  function onLoad(splineApp: Application) {
+    model.current = splineApp.findObjectByName("ghost");
+
+    if (model.current) {
+      // Zoom out a bit
+      model.current!.scale.x -= 0.2;
+      model.current!.scale.y -= 0.2;
+      model.current!.scale.z -= 0.2;
+
+      const x = model.current!.rotation.x;
+      const y = model.current!.rotation.y;
+      const z = model.current!.rotation.z;
+
+      setOriginalX(x);
+      setOriginalY(y);
+      setOriginalZ(z);
+    }
+  }
   return (
     <PageLayout>
-      <Box mt={"4rem"}>
+      <Box mt={"4rem"} position={"relative"}>
+        <Box height={"40rem"} position={"absolute"} inset={0} zIndex={-20}>
+          <Spline scene={"/rocket.spline"} onLoad={onLoad} />
+        </Box>
         <Heading
           fontSize={"2.4rem"}
           fontWeight={"extrabold"}
@@ -18,7 +64,7 @@ export default function Home(): React.ReactNode {
         </Heading>
         <Text
           fontSize={"1.2rem"}
-          fontWeight={"medium"}
+          fontWeight={"extrabold"}
           textAlign="center"
           mt={"6"}
         >
@@ -31,9 +77,9 @@ export default function Home(): React.ReactNode {
             <Button colorScheme="blue">🎩 Experience Magic</Button>
           </Link>
         </Flex>
-      </Box>
-      <Box mt={20}>
-        <Quote />
+        <Box mt={20}>
+          <Quote />
+        </Box>
       </Box>
     </PageLayout>
   );
